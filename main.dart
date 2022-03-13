582f0abdaf09cf6246d057da2e2410fee6cead91
Abdallah1Habib <94358402+Abdallah1Habib@users.noreply.github.com>
2022-03-13 09:26:16 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,10 @@
 /*
-the main dart file for our project 
-still empty until we finish learning about flutter 
+the main dart file for our project  
 Team members:
-1- Abdallah Mohamed Habib
-2- Ahmed Sayed Abodeif
-3- Hazem Abobaker
-4- Mohamed Abdelaleim
-5- Mohamed Gaber
-6- Ziad Zakaria
+1- Abdallah Mohamed Habib (Team Leader)
+2- Ahmed Sayed Abodeif (Front End)
+3- Hazem Abobaker (Back End)
+4- Mohamed Abdelaleim (Front End)
+5- Mohamed Gaber (Back End)
+6- Ziad Zakaria (Back End)
 */
